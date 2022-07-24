2a17375a46298f412bfcab28b8ea76b5476f8756
neurix <redfroglet@gamil.com>
2022-07-24 22:13:36 +0900

    feat: Dijkstra's

--- dataSturctures/WeightedGraph.js
+++ dataSturctures/WeightedGraph.js
@@ -1,3 +1,5 @@
+import { PriorityQueue } from "./PriorityQueue";
+
 export class WeightedGraph {
 	constructor(){
 		this.adjacencyList = {}
@@ -12,5 +14,57 @@ export class WeightedGraph {
 	}
 	addVertex(vertex){
 		if(!this.adjacencyList[vertex]) this.adjacencyList[vertex] = []
+	}
+	shortestPath(vertexStart, vertexEnd){
+		/*
+				Function takes to vertexes and finds the shortest distance between the two.
+				Inputs: vertex one and vertex two
+				Outputs: the path
+				Can the outputs be derived frm the inputs?:
+				Label of important data: queue (which determines the data value to choose next, visited, shortest path array (array with the shortest path to a given point)
+		 */
+		// declare visited object
+		let visited = {}
+		// declare shortestPath object and set defaults with the start point to zero and all other distances to zero and prev to null
+		const shortestPathObject = {}
+		Object.keys(this.adjacencyList).forEach((key) => {
+			shortestPathObject[key] = {distance: Infinity, prev:null }
+		})
+		shortestPathObject[vertexStart].distance = 0
+		// declare queue
+		let priorityQueue = new PriorityQueue()
+		// put VertexSTart in the queue
+		priorityQueue.enqueue(vertexStart, 0)
+		// put vertex start in the visited object
+
+		// loop while queue not empty
+		while(priorityQueue.values.length){
+			// dequeue vertex
+			const visitedVertex = priorityQueue.dequeue()?.data
+			visited[visitedVertex] = true
+			// loop through neighbors
+			const neighbors = this.adjacencyList[visitedVertex] || []
+			for(const neighbor of neighbors){
+				if(!visited[neighbor.node]){ 		// if neighbor hasn't been visited then
+					// compare the total distance from the starting point and if it is lower
+					const distance = neighbor.weight + shortestPathObject[visitedVertex].distance
+					if(distance < shortestPathObject[neighbor.node].distance){
+						// set the value of its shortest distance with the new value and set its prev to the dequeued vertex
+						shortestPathObject[neighbor.node].distance = distance
+						shortestPathObject[neighbor.node].prev = visitedVertex
+						priorityQueue.enqueue(neighbor.node, distance)
+					}
+				}
+			}
+		}
+		// build shortest patjh array and return it.
+		let backTrackVertex = shortestPathObject[vertexEnd]
+		let shortestPath = [vertexEnd]
+		while(backTrackVertex.prev){
+			shortestPath.push(backTrackVertex.prev)
+			backTrackVertex = shortestPathObject[backTrackVertex.prev]
+		}
+		// declare shortest path array and
+		return shortestPath.reverse()
 	}
 }
--- dataSturctures/test/weightedGraph.test.js
+++ dataSturctures/test/weightedGraph.test.js
@@ -12,6 +12,22 @@ it('can add vertex', () => {
 	wGraph.addEdge('A', 'C', 7)
 	expect(wGraph.adjacencyList['A']).toStrictEqual([{ node:'B', weight: 3}, { node:'C', weight: 7}])
 	expect(wGraph.adjacencyList['B']).toStrictEqual([{ node:'A', weight: 3}])
+})
 
-	console.log(wGraph.adjacencyList)
+
+it('shortest path', () => {
+	const arr = ['A', 'B', 'C', 'D', 'E', 'F']
+	let wGraph = new WeightedGraph()
+	for(const vertex of arr) wGraph.addVertex(vertex)
+	wGraph.addEdge('A', 'B', 4)
+	wGraph.addEdge('A', 'C', 2)
+	wGraph.addEdge('B', 'E', 3)
+	wGraph.addEdge('C', 'D', 2)
+	wGraph.addEdge('C', 'F', 4)
+	wGraph.addEdge('D', 'F', 1)
+	wGraph.addEdge('D', 'E', 3)
+	wGraph.addEdge('F', 'E', 1)
+	expect( wGraph.shortestPath('A', 'E')).toStrictEqual(['A', 'C', 'D', 'F', 'E'])
+	expect( wGraph.shortestPath('A', 'D')).toStrictEqual(['A', 'C', 'D'])
+	expect( wGraph.shortestPath('C', 'B')).toStrictEqual(['C', 'A', 'B'])
 })
