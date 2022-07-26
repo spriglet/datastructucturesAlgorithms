19ef36692ac006f6ddba8573313ead227514967e
neurix <redfroglet@gamil.com>
2022-07-26 18:17:49 +0900

    feat: Prisoners Pick

--- algorithms/PrisonerPickNumber.js
+++ algorithms/PrisonerPickNumber.js
@@ -28,7 +28,7 @@ const randomSortedNumbers = (choices) => {
 const didAllPrisonersFindTheirNumber = (numberOfPrisoners) => {
 	if(numberOfPrisoners % 2 === 1) return 'Number of prisoners must be even'
 	const boxes = randomSortedNumbers(numberOfPrisoners)
-	let prisoners = []
+	let prisonerData = []
 	for(let i = 0; i<numberOfPrisoners;i++){
 		let numberOfChoices  = 0;
 		let choice = i
@@ -38,19 +38,51 @@ const didAllPrisonersFindTheirNumber = (numberOfPrisoners) => {
 			numberOfChoices++
 			if(number === i){
 				path.push(number)
-				prisoners.push({numberOfTries: numberOfChoices, success:  numberOfChoices <= (numberOfPrisoners / 2), path, prisonerNumber: i } )
+				prisonerData.push({numberOfTries: numberOfChoices, success:  numberOfChoices <= (numberOfPrisoners / 2), path, prisonerNumber: i } )
 				break;
 			}
 			path.push(number)
 			choice = number
 		}
 	}
-	return prisoners.filter((obj) => obj.success).length === numberOfPrisoners ? true : false
+	return {boxes, prisonerData}
 }
 let numberOfSuccess = 0
-const numberIterations = 10000
+const numberIterations = 1
+const numberOfPrisoners = 100
+let bulkData = []
 for(let i = 0; i < numberIterations; i++){
-	const success = didAllPrisonersFindTheirNumber(100)
-	if(success) numberOfSuccess++
+
+	const data = didAllPrisonersFindTheirNumber(numberOfPrisoners)
+	if(data.prisonerData.filter((obj) => obj.success).length === numberOfPrisoners ? true : false) numberOfSuccess++
+  const {boxes, prisonerData} = data
+	const stats = {iteration: 0, loops: [], randomNumberedBoxes: null, randomNumberedBoxes:boxes}
+	let longestLoop = { prisonerNumber:null, length:null, path: null }
+	for(let j = 0 ; j < numberOfPrisoners; j++){
+		const { path } = prisonerData[j]
+		longestLoop.prisonerNumber = j
+		longestLoop.length = path.length
+		longestLoop.path = path
+		const sort1 = [...path].sort((a, b) => a - b)
+		let pathExist = false
+		for(const path of stats.loops){
+			const sort2 = [...path].sort((a, b) => a - b)
+			if(sort2.toString() === sort1.toString()){
+				pathExist = true
+				break;
+			}
+		}
+		if(!pathExist) stats.loops.push(path)
+	}
+	bulkData.push(stats)
+}
+for(const stats of bulkData){
+	const {loops} = stats
+	console.log('boxes', stats.randomNumberedBoxes)
+	console.log('number of loops', loops.length)
+	for(const loop of loops){
+		console.log('loop length', loop.length)
+		console.log('path of the loop', loop)
+	}
 }
 console.log((100 * (numberOfSuccess / numberIterations)).toString() + '%')
